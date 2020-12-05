961ebfb33611551b2b6967141738a1580453f172
Benedikt Jóhannesson <bennigeir@users.noreply.github.com>
2020-12-05 11:45:04 +0000

    Preprocess update

--- code/preprocess.py
+++ code/preprocess.py
@@ -6,6 +6,12 @@ Created on Fri Dec  4 19:56:31 2020
 """
 
 import pandas as pd
+import re
+
+from nltk.tokenize import word_tokenize
+'''from utils import (remove_url,
+                   remove_non_alpha,
+                   )'''
 
 
 class PreprocessTweets():
@@ -23,10 +29,9 @@ class PreprocessTweets():
                          'OriginalTweet']
         self.target = ['Sentiment']
         
-        self.X_train = None
-        self.X_test = None
-        self.y_train = None
-        self.y_test = None
+        self.X_train = self.X_test = self.y_train = self.y_test = None
+        
+        self.max_seq_len = 50
         
     
     def load_data(self):
@@ -37,30 +42,79 @@ class PreprocessTweets():
         
         # Split data into features and targets
         self.X_train = train_data[self.features]
-        self.X_test = test_data[self.target]
+        self.y_train = train_data[self.target]
         
-        self.y_train = train_data[self.features]
+        self.X_test = test_data[self.features]
         self.y_test = test_data[self.target]
         
         return self.X_train, self.X_test, self.y_train, self.y_test
     
     
-    def clean_data(self):
-        # lower, or upper, or both?
-        # remoce special characters?
-        pass
+    def remove_url(self, tweet):
+        return (re.sub(r'http\S+', '', tweet))
     
+
+    def remove_non_alpha(self, tweet):
+        return (re.sub(r'[^\x20-\x7E]', '', tweet))
+    
+    
+    def clean_data(self):
+        
+        self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.remove_url(x))
+        self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.remove_url(x))
+        
+        self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.remove_non_alpha(x))
+        self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.remove_non_alpha(x))
+        
+        # Cast tweets to lower case
+        self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].str.lower()
+        self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].str.lower()
+        
+        return self.X_train, self.X_test, self.y_train, self.y_test
+
+
+    def pad_list(self, l):
+        while len(l) <= self.max_seq_len:
+            l.append('0')
+        return l
+    
+    
+    def trim_list(self, l):
+        return l[0:self.max_seq_len]
+
     
     def tokenize(self):
         # Use nltk tokenize?
-        pass
+        self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: word_tokenize(x))
+        self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: word_tokenize(x))
+    
+        return self.X_train, self.X_test, self.y_train, self.y_test
+    
+    
+    def trimming(self):
+
+        self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.trim_list(x))
+        self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.trim_list(x))
+        
+        return self.X_train, self.X_test, self.y_train, self.y_test 
     
     
     def padding(self):
-        # Tweets have different lengths...
-        pass
+        
+        self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.pad_list(x))
+        self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.pad_list(x))
+        
+        return self.X_train, self.X_test, self.y_train, self.y_test 
     
     
     def vocabulary(self):
         # Frequency distribution?
         pass
+
+
+prepro = PreprocessTweets()
+X_train, X_test, y_train, y_test = prepro.load_data()
+X_train, X_test, y_train, y_test = prepro.clean_data()
+X_train, X_test, y_train, y_test = prepro.tokenize()
+X_train, X_test, y_train, y_test = prepro.trimming()
+X_train, X_test, y_train, y_test = prepro.padding()
---
+++ code/utils/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 11:12:49 2020
+
+@author: Benedikt
+"""
+
+from .util import (remove_url,
+                   remove_non_alpha)
