1c2af05e6ceb98dcb4082adc43a995bf8e543ecc
Sara Árnadóttir <saraarna@Saras-MacBook-Pro-2.local>
2020-12-05 14:17:07 +0000

    stuff

--- code/model.py
+++ code/model.py
@@ -1,11 +1,103 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec  5 09:55:19 2020
+import torch
+import torchvision
+from torchvision import transforms, datasets
+import torch.nn.functional as functional
+import torch.nn as nn
+from preprocess import PreprocessTweets
+import pandas as pd
 
-@author: Benedikt
-"""
+from typing import Union
+from torch.utils.data import DataLoader
+
+# class FancyModel():
+#     # INSERT MODEL HERE PLZ
+#     pass
+
+BATCH_SIZE = 500
+
+class CNN():
+    def __init__(self):
+        super().__init__()
+
+        self.layer_1 = nn.Sequential(
+            nn.Conv2d(1, 51, kernel_size=5, stride=1, padding=2),  # 10 x (28 x 28)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)  # 14 x 14
+        )
+
+        self.layer_2 = nn.Sequential(
+            nn.Conv2d(51, 102, kernel_size=5, stride=1, padding=2),  # 20 x (14 x 14)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.flatten = nn.Flatten()
+        self.layer_3 = nn.Linear(20 * 7 * 7, 64)
+        self.relu = nn.ReLU()
+        self.layer_4 = nn.Linear(64, 10)
+        return
+
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = self.layer_2(x)
+        x = self.flatten(x)
+        x = self.layer_3(x)
+        x = self.relu(x)
+        x = self.layer_4(x)
+        return functional.log_softmax(x, dim=1)
 
 
-class FancyModel():
-    # INSERT MODEL HERE PLZ
-    pass
+net = CNN()
+print(net)
+
+
+def get_train_test() -> Union[DataLoader, DataLoader]:
+    pt = PreprocessTweets()
+    X_train, X_test, y_train, y_test = pt.process()
+
+    benni = X_train.to_numpy()
+
+    sara = torch.Tensor(benni)
+    emil = torch.Tensor(y_train.to_numpy());
+    train_data = torch.utils.data.TensorDataset(sara, emil)
+
+
+    test_data = torch.utils.data.TensorDataset(torch.Tensor(X_test.to_numpy()),
+                                               torch.Tensor(y_test.to_numpy()))
+
+    train_loader = DataLoader(train_data, batch_size=BATCH_SIZE,
+                              shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=BATCH_SIZE,
+                             shuffle=False)
+
+    return train_loader, test_loader
+
+
+
+training_set, test_set = get_train_test()
+
+
+# Train the network
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+for epocs in range(2):
+  for data in training_set:
+    X, y = data
+    optimizer.zero_grad()
+    output = net(X)
+    loss = functional.nll_loss(output, y)
+    loss.backward()
+    optimizer.step()
+  print(loss)
+
+  # Evaluate.
+  total = 0
+  correct = 0
+  with torch.no_grad():
+      for data in test_set:
+          X, y = data;
+          output = net(X)  # Forward pass
+          for idx, val in enumerate(output):
+              if (torch.argmax(val) == y[idx]):
+                  correct += 1
+              total += 1
+  print("Accuracy: ", round(correct / total, 3))
--- code/preprocess.py
+++ code/preprocess.py
@@ -9,112 +9,106 @@ import pandas as pd
 import re
 
 from nltk.tokenize import word_tokenize
+
 '''from utils import (remove_url,
                    remove_non_alpha,
                    )'''
 
 
 class PreprocessTweets():
-    
+
     def __init__(self):
-        
         self.train_path = '../data/Corona_NLP_train.csv'
         self.test_path = '../data/Corona_NLP_test.csv'
         self.encoding = 'ISO-8859-1'
-        
+
         self.features = ['UserName',
                          'ScreenName',
                          'Location',
                          'TweetAt',
                          'OriginalTweet']
         self.target = ['Sentiment']
-        
+
         self.X_train = self.X_test = self.y_train = self.y_test = None
-        
+
         self.max_seq_len = 50
-        
-    
+
     def load_data(self):
-        
         # Read raw csv files
-        train_data = pd.read_csv(self.train_path, encoding = self.encoding)
-        test_data = pd.read_csv(self.test_path, encoding = self.encoding)
-        
+        train_data = pd.read_csv(self.train_path, encoding=self.encoding)
+        test_data = pd.read_csv(self.test_path, encoding=self.encoding)
+
         # Split data into features and targets
         self.X_train = train_data[self.features]
         self.y_train = train_data[self.target]
-        
+
         self.X_test = test_data[self.features]
         self.y_test = test_data[self.target]
-        
+
         return self.X_train, self.X_test, self.y_train, self.y_test
-    
-    
+
     def remove_url(self, tweet):
-        return (re.sub(r'http\S+', '', tweet))
-    
+        return re.sub(r'http\S+', '', tweet)
 
     def remove_non_alpha(self, tweet):
-        return (re.sub(r'[^\x20-\x7E]', '', tweet))
-    
-    
+        return re.sub(r'[^\x20-\x7E]', '', tweet)
+
     def clean_data(self):
-        
         self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.remove_url(x))
         self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.remove_url(x))
-        
+
         self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.remove_non_alpha(x))
         self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.remove_non_alpha(x))
-        
+
         # Cast tweets to lower case
         self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].str.lower()
         self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].str.lower()
-        
-        return self.X_train, self.X_test, self.y_train, self.y_test
 
+        return self.X_train, self.X_test, self.y_train, self.y_test
 
     def pad_list(self, l):
         while len(l) <= self.max_seq_len:
             l.append('0')
         return l
-    
-    
+
     def trim_list(self, l):
         return l[0:self.max_seq_len]
 
-    
     def tokenize(self):
         # Use nltk tokenize?
         self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: word_tokenize(x))
         self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: word_tokenize(x))
-    
-        return self.X_train, self.X_test, self.y_train, self.y_test
-    
-    
-    def trimming(self):
 
+        return self.X_train, self.X_test, self.y_train, self.y_test
+
+    def trimming(self):
         self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.trim_list(x))
         self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.trim_list(x))
-        
-        return self.X_train, self.X_test, self.y_train, self.y_test 
-    
-    
+
+        return self.X_train, self.X_test, self.y_train, self.y_test
+
     def padding(self):
-        
         self.X_train['OriginalTweet'] = self.X_train['OriginalTweet'].apply(lambda x: self.pad_list(x))
         self.X_test['OriginalTweet'] = self.X_test['OriginalTweet'].apply(lambda x: self.pad_list(x))
-        
-        return self.X_train, self.X_test, self.y_train, self.y_test 
-    
-    
+
+        return self.X_train, self.X_test, self.y_train, self.y_test
+
     def vocabulary(self):
         # Frequency distribution?
         pass
 
+    def process(self):
+        self.load_data()
+        self.clean_data()
+        self.tokenize()
+        self.trimming()
+        self.padding()
+        return self.X_train['OriginalTweet'] , self.X_test['OriginalTweet'] , self.y_train['OriginalTweet'] , self.y_test['OriginalTweet']
 
-prepro = PreprocessTweets()
-X_train, X_test, y_train, y_test = prepro.load_data()
-X_train, X_test, y_train, y_test = prepro.clean_data()
-X_train, X_test, y_train, y_test = prepro.tokenize()
-X_train, X_test, y_train, y_test = prepro.trimming()
-X_train, X_test, y_train, y_test = prepro.padding()
+
+# prepro = PreprocessTweets()
+# X_train, X_test, y_train, y_test = prepro.load_data()
+# X_train, X_test, y_train, y_test = prepro.clean_data()
+# X_train, X_test, y_train, y_test = prepro.tokenize()
+# X_train, X_test, y_train, y_test = prepro.trimming()
+# X_train, X_test, y_train, y_test = prepro.padding()
